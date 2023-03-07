01ad13251995bb845038eb23ded3ae4c6c7dd5c7
sakda homhuan <sakda.homhuan@gmail.com>
2023-03-07 09:10:43 +0700

    update legend and geolocation

--- nodejs/www/aqi/app.js
+++ nodejs/www/aqi/app.js
@@ -54,12 +54,6 @@ function loadMap() {
         subdomains: ["mt0", "mt1", "mt2", "mt3"]
     });
 
-    // const pro = L.tileLayer.wms("https://rti2dss.com/geoserver/th/wms?", {
-    //     layers: "th:province_4326",
-    //     format: "image/png",
-    //     transparent: true
-    // });
-
     var baseMap = {
         'สีเทา': mapbox.addTo(map),
         'ภูมิประเทศ': gter,
@@ -68,7 +62,7 @@ function loadMap() {
     };
 
     var overLayer = {
-        "จุดความร้อน": fc.addTo(map),
+        "จุดตรวจวัด AQI": fc.addTo(map),
         // '<img src="https://rti2dss.com/geoserver/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=20&HEIGHT=20&LAYER=th:province_4326" /> จังหวัด': pro
     }
 
@@ -76,20 +70,8 @@ function loadMap() {
 }
 
 function onLocationFound(e) {
-    console.log(e)
-    // gps = L.marker(e.latlng);
-    var point = turf.point([e.latlng.lng, e.latlng.lat]);
-    var buffered = turf.buffer(point, 20, { units: 'kilometers' });
-    var bbox = turf.bbox(buffered);
-    map.fitBounds([
-        [bbox[1], bbox[0]],
-        [bbox[3], bbox[2]]
-    ]);
-
-    let token = "2b9b7d19f47c41ab2f58a00c0f61315f7a0c5926"
-    // GET https://api.waqi.info/v2/map/bounds?latlng={{minLat}},{{minLng}},{{maxLat}},{{maxLng}}&networks=all&token={{token}}
-    let aqiData = axios.get(`https://api.waqi.info/v2/map/bounds?latlng=${bbox[1]},${bbox[0]},${bbox[3]},${bbox[2]}&networks=all&token=${token}`);
-    loadHotspot(aqiData);
+    document.getElementById("lat").value = e.latlng.lat;
+    document.getElementById("lng").value = e.latlng.lng;
 }
 
 function onLocationError(e) {
@@ -100,7 +82,14 @@ function refreshPage() {
     location.reload(true);
 }
 
-map.on("locationfound", onLocationFound);
+// map.on("locationfound", onLocationFound);
+// map.locate({ setView: true, maxZoom: 14 });
+navigator.geolocation.getCurrentPosition(function (position) {
+    // map.setView([position.coords.latitude, position.coords.longitude], 14);
+    document.getElementById("lat").value = position.coords.latitude;
+    document.getElementById("lng").value = position.coords.longitude;
+    getLBS();
+});
 
 var lc = L.control.locate({
     position: "topleft",
@@ -108,37 +97,86 @@ var lc = L.control.locate({
         title: "enable gps"
     },
     locateOptions: {
-        maxZoom: 8,
+        maxZoom: 18,
         enableHighAccuracy: true
     }
 }).addTo(map);
 
 lc.start();
 
-
-let loadHotspot = async (aqiData) => {
+let getLBS = () => {
     map.eachLayer(function (layer) {
         if (layer.options.name == "lyr") {
             map.removeLayer(layer);
         }
     });
+    // console.log("getLBS");
+    let lat = document.getElementById("lat").value;
+    let lng = document.getElementById("lng").value;
+    var point = turf.point([Number(lng), Number(lat)]);
+    var buffered = turf.buffer(point, 20, { units: 'kilometers' });
+    var bbox = turf.bbox(buffered);
+    // map.fitBounds([
+    //     [bbox[1], bbox[0]],
+    //     [bbox[3], bbox[2]]
+    // ]);
 
-    let aqi = await aqiData;
-    aqi.data.data.forEach(e => {
-        var color = e.aqi <= 50 ? "green" : e.aqi <= 100 ? "yellow" : e.aqi <= 150 ? "orange" : e.aqi <= 200 ? "red" : e.aqi <= 300 ? "purple" : "maroon";
-        console.log(color);
-        L.circleMarker([e.lat, e.lon], {
-            radius: 5,
-            color: color,
-            name: "lyr"
-        }).bindPopup(`<div class="kanit"><b>${e.station.name}</b><br/>AQI: ${e.aqi}</div>`).addTo(fc);
-    });
-    myModal.hide();
+    let token = "2b9b7d19f47c41ab2f58a00c0f61315f7a0c5926"
+    // GET https://api.waqi.info/v2/map/bounds?latlng={{minLat}},{{minLng}},{{maxLat}},{{maxLng}}&networks=all&token={{token}}
+    axios.get(`https://api.waqi.info/v2/map/bounds?latlng=${bbox[1]},${bbox[0]},${bbox[3]},${bbox[2]}&networks=all&token=${token}`)
+        .then(res => {
+            console.log(res);
+            res.data.data.forEach(e => {
+                var color = e.aqi <= 50 ? "#009966" : e.aqi <= 100 ? "#ffde33" : e.aqi <= 150 ? "#ff9933" : e.aqi <= 200 ? "#cc0033" : e.aqi <= 300 ? "#660099" : "#7e0023";
+                // console.log(color);
+                L.circleMarker([e.lat, e.lon], {
+                    radius: 5,
+                    color: color,
+                    name: "lyr"
+                }).bindPopup(`<div class="kanit"><b>${e.station.name}</b><br/>AQI: ${e.aqi}</div>`).addTo(fc);
+            });
+            myModal.hide();
+        })
 }
 
 var myModal = new bootstrap.Modal(document.getElementById('myModal'), {
     keyboard: false,
 })
 
+/*Legend specific*/
+var legend = L.control({ position: "bottomleft" });
 
-initializeLiff()  
+function showLegend() {
+    legend.onAdd = function (map) {
+        var div = L.DomUtil.create("div", "legend");
+        div.innerHTML += `<button class="btn btn-sm" onClick="hideLegend()">
+      <span class="kanit">ซ่อนสัญลักษณ์</span><i class="fa fa-angle-double-down" aria-hidden="true"></i>
+    </button><br>`;
+        // div.innerHTML += "<h4>Tegnforklaring</h4>";
+        div.innerHTML += '<i style="background: #009966"></i><span class="kanit">อากาศดี</span><br>';
+        div.innerHTML += '<i style="background: #ffde33"></i><span class="kanit">อากาศดีปานกลาง</span><br>';
+        div.innerHTML += '<i style="background: #ff9933"></i><span class="kanit">อากาศเริ่มไม่ดี</span><br>';
+        div.innerHTML += '<i style="background: #cc0033"></i><span class="kanit">อากาศไม่ดี</span><br>';
+        div.innerHTML += '<i style="background: #660099"></i><span class="kanit">อากาศไม่มีอย่างยิ่ง</span><br>';
+        div.innerHTML += '<i style="background: #7e0023"></i><span class="kanit">อันตราย</span><br>';
+        // div.innerHTML += '<i style="background: #bd0000"></i><span class="kanit">ฝนตกหนักมาก</span><br>';
+        return div;
+    };
+    legend.addTo(map);
+}
+
+function hideLegend() {
+    legend.onAdd = function (map) {
+        var div = L.DomUtil.create('div', 'info legend')
+        div.innerHTML += `<button class="btn btn-sm" onClick="showLegend()">
+        <small class="prompt"><span class="kanit">แสดงสัญลักษณ์</span></small> 
+        <i class="fa fa-angle-double-up" aria-hidden="true"></i>
+    </button><br> `;
+        return div;
+    };
+    legend.addTo(map);
+}
+
+// hideLegend()
+initializeLiff()
+
